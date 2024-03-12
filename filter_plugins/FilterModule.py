bfb627f3b453991624986157bc52f69828b01bca
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2024-03-13 01:35:00 +0900

    Updated

--- filter_plugins/FilterModule.py
+++ filter_plugins/FilterModule.py
@@ -6,11 +6,11 @@ class FilterModule(object):
             'pretty_yaml': self.pretty_yaml,
         }
 
-    def pretry_yaml(self, d, indent=0, result=""):
+    def pretty_yaml(self, d, indent=0, result=""):
         for key, value in d.items():
             result += " " * indent + str(key)
             if isinstance(value, dict):
-                result = self.pretty(value, indent=(indent + 2), result=result + ":\n")
+                result = self.pretty_yaml(value, indent=(indent + 2), result=result + ":\n")
             else:
                 result += ": " + str(value) + "\n"
         return result
